fdb45323570317d585f8159320463c9a326e9a78
Oleksandr Smirnov <olexsmir@gmail.com>
2025-11-30 22:38:18 +0200

    feat: site

---
+++ lua/blog/config.lua
@@ -0,0 +1,21 @@
+local config = {}
+
+config.name = "olexsmir"
+config.title = "olexsmir's blog"
+config.email = "olexsmir@gmail.com"
+config.cname = "olexsmir.xyz"
+config.url = "https://" .. config.cname
+config.feed = {
+  url = config.url .. "/feed.xml",
+  subtitle = "olexsmir's blog feed, also hi rss reader!",
+}
+
+config.build = {
+  chroma_theme = "tokyonight-night",
+
+  output = "build",
+  assets = "assets",
+  posts = "posts",
+}
+
+return config
